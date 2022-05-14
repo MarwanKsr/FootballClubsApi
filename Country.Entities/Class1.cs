baacfc3c8130b9fe7fd4238f35dcbb4a9307e6d3
Marwan <Marwan.kaseer1997@gmail.com>
2022-05-14 22:33:40 +0300

    product is done

---
+++ Country.Entities/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Country.Entities
+{
+    public class Country
+    {
+
+    }
+}
---
+++ Country.Entities/Club.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country.Entities
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfEstablishment { get; set; }
+        public string? StadiumName { get; set; }
+        public string CoachsName { get; set; }
+        public string? PresidentsName { get; set; }
+        public Country Country { get; set; }
+        public int NumberOfLeagueTitles { get; set; }
+        public int NumberOfCupTitles { get; set; }
+        public int NumberOfChampionsLeagueTitles { get; set; }
+
+
+
+
+    }
+}
